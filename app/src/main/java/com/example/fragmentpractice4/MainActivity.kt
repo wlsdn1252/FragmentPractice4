9896cf4b845e640589a52e39d597f341ec48e0e3
강진우 <wlsdn1252@naver.com>
2023-01-17 16:31:57 +0900

    메인 액티비티에서 화면에 뷰페이저랑 탭레이아웃 구현

--- app/src/main/java/com/example/fragmentpractice4/MainActivity.kt
+++ app/src/main/java/com/example/fragmentpractice4/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.fragmentpractice4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentpractice4.adapters.FragmentAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : FragmentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAdapter = FragmentAdapter(supportFragmentManager)
+
+        mainViewPager.adapter = mAdapter    // 여기까지가 뷰페이저랑 프래그먼트 연결
+
+        // 뷰페이저랑 탭레이아웃 연결
+        mainTabLayout.setupWithViewPager(mainViewPager)
     }
 }
