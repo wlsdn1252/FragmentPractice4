0785233e1596c924c1b1bb97dea1ccffc9c02d1a
강진우 <wlsdn1252@naver.com>
2023-01-17 17:03:12 +0900

    간단한 동작을 위해 첫번째 프래그먼트의 onActivityCreated함수에 코드 작성 mainActivity에 작성하면 앱이 튕긴다

--- app/src/main/java/com/example/fragmentpractice4/MainActivity.kt
+++ app/src/main/java/com/example/fragmentpractice4/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.fragmentpractice4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fragmentpractice4.adapters.FragmentAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mAdapter : FragmentAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         mAdapter = FragmentAdapter(supportFragmentManager)
 
@@ -19,5 +25,9 @@ class MainActivity : AppCompatActivity() {
 
         // 뷰페이저랑 탭레이아웃 연결
         mainTabLayout.setupWithViewPager(mainViewPager)
+
+
+
     }
+
 }
--- app/src/main/java/com/example/fragmentpractice4/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice4/fragments/FirstFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice4.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -19,6 +21,11 @@ class FirstFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        mainBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "메세지가 변경되었습니다.", Toast.LENGTH_SHORT).show()
+            reTxt.text = "텍스트가 변경 되었습니다."
+        }
     }
 
 }
